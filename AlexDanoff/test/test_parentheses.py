6b4b967916037c5291e4fc8291d018ce66f553ba
physikerwelt <wiki@physikerwelt.de>
2016-04-05 13:15:15 -0400

    Fix warnings from QuantifiedCode

--- AlexDanoff/test/test_parentheses.py
+++ AlexDanoff/test/test_parentheses.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from parentheses import insert, remove
 
-testCases = [{
+test_cases = [{
     'normal': '\\sin(x)',
     'replaced': '\\sin###open_0###x###close_0###',
 }, {
@@ -15,7 +15,7 @@ testCases = [{
     }
 ]
 
-roundTripTests = [{
+round_trip_tests = [{
     'normal': '\\sin(\\arcsin(x))',
 }, {
     'normal': '\\sin{\\arcsin(x)}',
@@ -31,19 +31,19 @@ roundTripTests = [{
 
 class TestInsert(TestCase):
     def test_insert(self):
-        for case in testCases:
+        for case in test_cases:
             self.assertEqual(case['normal'], insert(case['replaced'], case.get('curly', False)))
 
     def test_remove(self):
-        for case in testCases:
+        for case in test_cases:
             self.assertEqual(case['replaced'], remove(case['normal'], case.get('curly', False)))
 
     def test_round_trip(self):
-        for case in testCases:
+        for case in test_cases:
             output = insert(remove(case['normal'], case.get('curly', False)), case.get('curly', False))
             self.assertEqual(case['normal'], output)
 
     def test_extended_round_trip(self):
-        for case in roundTripTests:
+        for case in round_trip_tests:
             output = insert(remove(case['normal'], case.get('curly', False)), case.get('curly', False))
             self.assertEqual(case['normal'], output)
