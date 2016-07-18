5c970f7ccd5cc56493b071ba40a15cd34f32fedc
OksanaTk <tkachoksn@gmail.com>
2016-07-18 15:27:46 -0400

    Update test_replace_i.py
    changed function name from replace_special.to remove_special in test case

--- AlexDanoff/test/test_replace_i.py
+++ AlexDanoff/test/test_replace_i.py
@@ -5,5 +5,5 @@ from src.replace_special import remove_special
 class TestReplaceSpecial(TestCase):
     def test__replace_special(self):
         tex = 'z^a = \\underbrace{z \\cdot z \\cdots z}_{n \\text{ times}} = 1 / z^{-a}.'
-        result = replace_special(tex)
+        result = remove_special(tex)
         self.assertEqual(tex, result)
