f850eb256d588ac8f439d63b24e35886fb8ffaa8
Joon Bang <joonb3@gmail.com>
2016-08-04 14:48:16 -0400

    Clean up some code

--- main_page/src/main.py
+++ main_page/src/main.py
@@ -1,8 +1,111 @@
 __author__ = "Azeem Mohammed"
 __status__ = "Development"
-__credits__ = ["Azeem Mohammed", "Joon Bang"]
+__credits__ = ["Joon Bang", "Azeem Mohammed"]
 
-import os
+import time
+import symbols_list
+import mod_main
 
-if raw_input("Update main page? (y/n): ") != "n":
-    pass
+"""
+Order:
+- List
+- Symbols List
+- Headers
+- List Ways
+"""
+
+
+def generate_html(tag_name, options, text, spacing=True):
+    result = "<" + tag_name
+    if options != "":
+        result += " " + options
+    result += ">\n" + text + "\n</" + tag_name + ">\n"
+
+    if not spacing:
+        return result.replace("\n", " ")[:-1] + "\n"
+
+    return result
+
+
+def update_macro_list(text):
+    lines = text.split("\n")
+    definitions = list()
+
+    for line in lines:
+        if "'''Definition:" in line:
+            macro = line.split("'''")[1][11:]
+            element = "* [[Definition:" + macro + "|" + macro + "]]"
+            definitions.append(element)
+
+    def_contents = generate_html("div", "style=\"-moz-column-count:2; column-count:2;-webkit-column-count:2\"",
+                                 "\n".join(definitions))
+
+    start = text.find("== Definition Pages ==")
+    end = text.find("= Copyright =")
+
+    return text[:start + 23] + "\n" + def_contents + "\n" + text[end:], definitions
+
+
+def update_headers(text, definitions):
+    pages = text.split("drmf_eof")[:-1]
+
+    macros = list()
+    formatted = list()
+    for definition in definitions:
+        macros.append("Definition:" + definition.split("|")[1][:-2])
+        formatted.append("[[" + macros[-1].replace(" ", "_") + "|" + macros[-1] + "]]")
+
+    output = ""
+
+    i = 0
+    for page in pages:
+        if "'''Definition:" not in page:
+            output += page + "drmf_eof\n"
+            continue
+
+        if "<div id=\"drmf_head\">" in page:
+            page = page[page.find(">> </div>\n</div>") + len(">> </div>\n</div>"):]
+
+        if "<div id=\"drmf_foot\">" in page:
+            page = page[:page.find("<div id=\"drmf_foot\">")]
+
+        nav_section = generate_html("div", "id=\"alignleft\"", "<< " + formatted[i - 1], spacing=False) + \
+                      generate_html("div", "id=\"aligncenter\"", "[[Main_Page|Main Page]]", spacing=False) + \
+                      generate_html("div", "id=\"alignright\"", formatted[(i + 1) % len(formatted)] + " >>",
+                                     spacing=False)[:-1]
+
+        header = generate_html("div", "id=\"drmf_head\"", nav_section)[:-1]
+        footer = generate_html("div", "id=\"drmf_foot\"", nav_section)[:-1]
+
+        output += "drmf_bof\n" + "'''" + macros[i] + "'''\n" + header + page[:-1] + footer + "\ndrmf_eof\n"
+
+        i += 1
+
+    return output
+
+
+def main():
+    if raw_input("Update main page? (y/n): ") == "n":
+        return
+
+    # read contents
+    with open("main_page.mmd") as main_page:
+        text = main_page.read()
+
+        # create backup
+        local_time = time.asctime(time.localtime(time.time())).split()
+        local_time = "_" + '_'.join(local_time[1:3] + local_time[3:][::-1])
+        with open("backups/main_page" + local_time + ".mmd.bak", "w") as backup:
+            backup.write(text)
+
+    text, definitions = update_macro_list(text)
+    text = symbols_list.main(text)
+    text = update_headers(text, definitions)
+    text = mod_main.main(text)
+
+    with open("main_page.mmd", "w") as main_page:
+        main_page.write(text)
+
+
+if __name__ == '__main__':
+    main()
--- main_page/src/mod_main.py
+++ main_page/src/mod_main.py
@@ -1,125 +1,115 @@
 __author__ = "Azeem Mohammed"
 __status__ = "Development"
+__credits__ = ["Joon Bang", "Azeem Mohammed"]
 
-import copy
 import csv
 
-DEF_STRING = "'''Definition:"
+
+def atSort(l):
+    return (sorted(l, key=lambda x: x.count("@")))
 
 
-def at_sort(strings):
-    """Sorts by number of @ symbols in string."""
-
-    return sorted(copy.copy(strings), key=lambda ch: ch.count("@"))
-
-
-def remove_brackets_text(string):
-    left_brackets = 0
-    text = ""
-
-    for ch in string:
-        if ch == "[":
-            left_brackets += 1
-        elif ch == "]":
-            left_brackets -= 1
-        elif not left_brackets:
-            text += ch
-
-    return text
-
-
-def find_all_pos(info):
-    # TODO: what is info? what does pos mean???
-
-    original = info[0]
-    key_string = info[3]
-
-    before, key_string = key_string.split("|", 1)
-    keys = key_string.split(":")
-    ret = []
-
-    for key in keys:
-        if key in ["F", "FO", "O", "O1"]:
-            if "@" in original:
-                ret.append(original[:original.find("@")])
-                if key == "O":
-                    ret.append(original)
+def remOpt(p):
+    parse = True
+    ret = ""
+    for x in p:
+        if parse:
+            if x == "[":
+                parse = False
             else:
-                ret.append(original)
-        elif key == "FnO":  # remove optional parameters
-            if original.find("@") != -1:
-                ret.append(remove_brackets_text(original[0:original.find("@")]))
-            else:
-                ret.append(remove_brackets_text(original))
-        elif key in ["P", "PO"]:
-            ret.append(original.replace("@@", "@"))
-        elif key == "PnO":  # remove optional parameters
-            ret.append(remove_brackets_text(original.replace("@@", "@")))
-        elif key in ["nP", "nPO", "PS", "O2"]:
-            ret.append(original)
-        elif key == "nPnO":  # remove optional paramters
-            ret.append(remove_brackets_text(original))
-
-        if "fo" in key:
-            ret.append(original.replace("@@@", "@" * int(key[2])))
-
-    return at_sort(ret), before
-
-with open("main_page.mmd") as main_page:
-    lines = main_page.read()
-
-lines = lines.replace("The LaTeX DLMF macro '''\\", "The LaTeX DLMF and DRMF macro '''\\")
-
-with open("Categories.txt") as categories:
-    cats = [category.strip() for category in categories.readlines()]
-
-text = ""
-i = 0
-
-while True:
-    n = lines.find(DEF_STRING, i)
-    if n == -1:
-        break
-    else:
-        macroN = lines[n + len(DEF_STRING):lines.find("'''", n + len(DEF_STRING))]
-        r = lines.find("'''\\", n)
-        q = lines.find("'''", r + len("'''\\"))
-
-        if lines[r:q].find("{") != -1:
-            lines = lines[:r] + "'''\\" + macroN + lines[q:]
-
-        if lines.find("\nThis macro is in the category of", n) < lines.find(DEF_STRING, n + 15) \
-                and "\nThis macro is in the category of" in lines[n:]:
-            p = lines.find("\nThis macro is in the category of", n)
+                ret += x
         else:
-            p = lines.find("\n", lines.find(".", n))
+            if x == "]":
+                parse = True
+    return ret
 
-        text += lines[i:p].rstrip() + "\n\n"
 
-        count = 0
-        list_calls = []
+def findAllPos(g):
+    key = g[3]
+    orig = g[0]
+    r = key[:key.find("|")]
+    key = key[key.find("|") + 1:]
+    keys = key.split(":")
+    ret = []
+    for x in keys:
+        if x == "F" or x == "FO" or x == "O" or x == "O1":
+            if orig.find("@") != -1:
+                ret.append(orig[0:orig.find("@")])
+                if x == "O":
+                    ret.append(orig)
+            else:
+                ret.append(orig)
+        if x == "FnO":  # remove optional parameters
+            if orig.find("@") != -1:
+                ret.append(remOpt(orig[0:orig.find("@")]))
+            else:
+                ret.append(remOpt(orig))
+        if x == "P" or x == "PO":
+            ret.append(orig.replace("@@", "@"))
+        if x == "PnO":  # remove optional parameters
+            ret.append(remOpt(orig.replace("@@", "@")))
+        if x == "nP" or x == "nPO" or x == "PS" or x == "O2":
+            ret.append(orig)
+        if x == "nPnO":  # remove optional paramters
+            ret.append(remOpt(orig))
+        if "fo" in x:
+            ret.append(orig.replace("@@@", "@" * int(x[2])))
+    return atSort(ret), r
 
-        glossary = csv.reader(open('new.Glossary.csv', 'rb'), delimiter=',', quotechar='\"')
-        for entry in glossary:
-            if not entry[0].find("\\" + macroN) and (len(entry[0]) == len(macroN) + 1 or not entry[0][len(macroN) + 1].isalpha()):
-                q, s = find_all_pos(entry)
-                list_calls += q
-                count += len(q)
 
-        for cat in cats:
-            if s + "    -" in cat:
-                category = "This macro is in the category of" + cat[cat.find("-") + 2:] + "\n\n"
-                break
+def main(lines):
+    lines = lines.replace("The LaTeX DLMF macro \'\'\'\\", "The LaTeX DLMF and DRMF macro \'\'\'\\")
+    flag = True
+    gCSV = csv.reader(open('new.Glossary.csv', 'rb'), delimiter=',', quotechar='\"')
+    toWrite = ""
+    i = 0
 
-        new = category + "In math mode, this macro can be called in the following way" + "s" * (count > 1) + ":\n\n"
+    categories = open("Categories.txt", "r")
+    cats = categories.readlines()
+    categories.close()
+    for x in range(0, len(cats)):
+        cats[x] = cats[x].strip()
+    while flag:
+        n = lines.find("\'\'\'Definition:", i)
+        if n == -1:
+            flag = False
+        else:
+            macroN = lines[n + len("\'\'\'Definition:"):lines.find("\'\'\'", n + len("\'\'\'Definition:"))]
+            r = lines.find("\'\'\'\\", n)
+            q = lines.find("\'\'\'", r + len("\'\'\'\\"))
+            if lines[r:q].find("{") != -1:
+                lines = lines[0:r] + "\'\'\'\\" + macroN + lines[q:]
 
-        for q in range(0, len(list_calls)):
-            c = list_calls[q]
-            new += ":'''" + c + "'''" + " produces <math>{\\displaystyle " + c + "}</math><br />\n"
+            if lines.find("\nThis macro is in the category of", n) < lines.find("\'\'\'Definition:", n + len(
+                    "\'\'\'Definition:") + 1) and lines.find("\nThis macro is in the category of", n) != -1:
+                # print n
+                p = lines.find("\nThis macro is in the category of", n)
+            else:
+                p = lines.find("\n", lines.find(".", n))
+            toWrite += lines[i:p]
+            toWrite = toWrite.rstrip()
+            toWrite += "\n\n"
+            count = 0
+            listCalls = []
+            gCSV = csv.reader(open('new.Glossary.csv', 'rb'), delimiter=',', quotechar='\"')
+            for g in gCSV:
+                if g[0].find("\\" + macroN) == 0 and (len(g[0]) == len(macroN) + 1
+                                                      or not g[0][len(macroN) + 1].isalpha()):
+                    q, s = findAllPos(g)
+                    listCalls += q
+                    count += len(q)
+            plural = 1  # if count>1: plural =1; if count=1: plural=0
+            if count == 1: plural = 0
+            for t in cats:
+                if s + "    -" in t:
+                    category = "This macro is in the category of" + t[t.find("-") + 2:]
+                    break
+            new = category + "\n\nIn math mode, this macro can be called in the following way" + "s" * plural + ":\n\n"
+            for q in range(0, len(listCalls)):
+                c = listCalls[q]
+                new += ":\'\'\'" + c + "\'\'\'" + " produces <math>{\\displaystyle " + c + "}</math><br />\n"
+            # Now add the multiple ways \macroname{n}@... produces <math>\macroname{n}@...</math>
+            toWrite += new + "\n"
+            i = lines.find("These are defined by", p)
 
-        # Now add the multiple ways \macroname{n}@... produces <math>\macroname{n}@...</math>
-        text += new + "\n"
-        i = lines.find("These are defined by", p)
-
-with open("main_page.mmd", "w") as main_page:
-    main_page.write(text + lines[i:])
+    return toWrite + lines[i:]
