0224c00e7735c80debaffc703e43ea0e1cef121b
KChen1250 <kchen1250@gmail.com>
2016-08-05 10:53:56 -0400

    Add test files

---
+++ eCF/test/test_removal_functions.py
@@ -0,0 +1,9 @@
+
+__author__ = 'Kevin Chen'
+__status__ = 'Development'
+
+from unittest import TestCase
+from MathematicaToLaTeX import remove_inactive
+from MathematicaToLaTeX import remove_conditionalexpression
+from MathematicaToLaTeX import remove_symbol
+
---
+++ eCF/test/test_single_macro_conversion_functions.py
@@ -0,0 +1,16 @@
+
+__author__ = 'Kevin Chen'
+__status__ = 'Development'
+
+from unittest import TestCase
+from MathematicaToLaTeX import beta
+from MathematicaToLaTeX import cfk
+from MathematicaToLaTeX import gamma
+from MathematicaToLaTeX import integrate
+from MathematicaToLaTeX import legendrep
+from MathematicaToLaTeX import legendreq
+from MathematicaToLaTeX import polyeulergamma
+from MathematicaToLaTeX import product
+from MathematicaToLaTeX import qpochhammer
+from MathematicaToLaTeX import summation
+
