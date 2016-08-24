ea61b6fd46272dccbf218b0623749e0a91a1f320
KChen1250 <kchen1250@gmail.com>
2016-08-24 16:11:13 -0400

    Remove import *

--- DLMF_preprocessing/test/test_latex_constraint_modifier.py
+++ DLMF_preprocessing/test/test_latex_constraint_modifier.py
@@ -3,9 +3,10 @@ __author__ = 'Kevin Chen'
 __status__ = 'Development'
 
 from unittest import TestCase
-from latex_constraint_modifier import *
+from latex_constraint_modifier import main
 
 import os
+
 PATHR = os.path.dirname(os.path.realpath(__file__)) + '/data/in.tex'
 PATHW = os.path.dirname(os.path.realpath(__file__)) + '/data/testout.tex'
 PATHC = os.path.dirname(os.path.realpath(__file__)) + '/data/correctout.tex'
